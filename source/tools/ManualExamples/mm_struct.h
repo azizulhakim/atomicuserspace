ae5aed204fb92b600d4480d00ea807501366494c
razib003 <mrazi003@fiu.edu>
2013-12-05 11:45:04 -0800

    Committing mm_struct.h

--- source/tools/ManualExamples/mm_struct.h
+++ source/tools/ManualExamples/mm_struct.h
@@ -1,7 +1,7 @@
 //#ifndef MM_STRUCT
 //#define unsigned int UINT32
 #include<stdio.h>
-#include "ll_list.h"
+//#include "ll_list.h"
 #if 0
 typedef struct virtual_mm{
 	int ml;
@@ -15,12 +15,37 @@ typedef struct list_vm{
 lvm *master=0;
 #endif
 
+#define VAL_SIZE 1
+typedef class virtual_mm{
+public:
+	unsigned int ml;//memory location
+    int type;// type
+	int state;// state number
+	//char val[VAL_SIZE];
+	char val;
+	virtual_mm* next;
+      //  virtual_mm* prev;
+	virtual_mm()
+	{
+		ml = 0;
+		type = -1;
+#if 0
+		for(int i = 0;i<VAL_SIZE;i++)
+		{
+			val[i] =0;
+		}
+#else
+		val = 0;
+#endif
+		next = 0;
+	}
+}vm;
 
 
 void save_data()
 {
 	//printf("test\n");
-        create_list();
+  //      create_list();
 }
 
 
